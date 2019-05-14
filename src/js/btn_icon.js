0904f0c3d54bc556b35afbf20a4f3c0634eda7f1
tubi <esteban.carrillo@sansano.usm.cl>
2019-05-14 19:24:23 -0400

    update

---
+++ src/js/btn_icon.js
@@ -0,0 +1,24 @@
+var icon_url = 'src/icons/{1}/{0}-{1}-{2}.svg';
+
+			$(document).ready(function() {
+				$('li.btn-icon').each(function() {
+					var element = $(this);
+					var icon = $(this).find('.iicon');
+					var values = icon.attr('class').split(' ')[1].split("-");
+
+					element.on({
+						'mouseenter' : function() {
+							icon.attr('src', icon_url.format(values[0], values[1], 'hover'));
+						},
+
+						'mouseleave' : function() {
+							icon.attr('src', icon_url.format(values[0], values[1], 'normal'));
+						},
+
+						'mousedown' : function() {
+							icon.attr('src', icon_url.format(values[0], values[1], 'click'));
+							window.location.replace('http://tubi_carrillo.github.io/src/' + icon.data('link'));
+						}
+					});
+				});
+			});
---
+++ src/js/functions.js
@@ -0,0 +1,4 @@
+String.prototype.format = function () {
+    var args = arguments;
+    return this.replace(/\{(\d+)\}/g, function (m, n) { return args[n]; });
+};
