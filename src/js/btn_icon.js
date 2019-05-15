72ab913227669a8c9259d114c8596fe9881e4cdc
tubi <esteban.carrillo@sansano.usm.cl>
2019-05-14 22:04:12 -0400

    update

--- src/js/btn_icon.js
+++ src/js/btn_icon.js
@@ -1,24 +1,24 @@
-var icon_url = 'src/icons/{1}/{0}-{1}-{2}.svg';
+var icon_url = '../../src/icons/{0}/icon-{0}-{1}.svg';
 
-			$(document).ready(function() {
-				$('li.btn-icon').each(function() {
-					var element = $(this);
-					var icon = $(this).find('.iicon');
-					var values = icon.attr('class').split(' ')[1].split("-");
+$(document).ready(function() {
+	$('li.btn-icon, .navbar.navbar-expand-lg.btn-icon').each(function() {
+		var element = $(this);
+		var icon = $(this).find('.iicon');
+		var name = icon.attr('class').split(' ')[1].split("-")[1];
 
-					element.on({
-						'mouseenter' : function() {
-							icon.attr('src', icon_url.format(values[0], values[1], 'hover'));
-						},
+		element.on({
+			'mouseenter' : function() {
+				icon.attr('src', icon_url.format(name, 'hover'));
+			},
 
-						'mouseleave' : function() {
-							icon.attr('src', icon_url.format(values[0], values[1], 'normal'));
-						},
+			'mouseleave' : function() {
+				icon.attr('src', icon_url.format(name, 'normal'));
+			},
 
-						'mousedown' : function() {
-							icon.attr('src', icon_url.format(values[0], values[1], 'click'));
-							window.location.replace('http://tubi-carrillo.github.io/' + icon.data('link'));
-						}
-					});
-				});
-			});
+			'mousedown' : function() {
+				icon.attr('src', icon_url.format(name, 'click'));
+				window.location.replace('http://tubi-carrillo.github.io/' + icon.data('link'));
+			}
+		});
+	});
+});
--- src/js/functions.js
+++ src/js/functions.js
@@ -2,3 +2,10 @@ String.prototype.format = function () {
     var args = arguments;
     return this.replace(/\{(\d+)\}/g, function (m, n) { return args[n]; });
 };
+
+
+function iconError(icon, name) {
+    icon.onerror = "";
+    icon.src = '../../src/icons/{0}/icon-{0}-normal.svg'.format(name);
+    return true;
+}
