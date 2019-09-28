c69efea2c3d03fab26bdfe6785a741cdd558103d
tubi <esteban.carrillo@sansano.usm.cl>
2019-09-27 23:50:29 -0300

    Update flatten.py

--- src/python/flatten.py
+++ src/python/flatten.py
@@ -4,20 +4,7 @@ from colorparse import paint
 from shutil import copy2 as cpy_file
 
 
-def _arg_parser():
-    parser = argparse.ArgumentParser()
-
-    parser.add_argument('type',
-                        help='the mode of icon loading, can be\
-                              either "all" (default), "regular"\
-                              or "filled".')
-
-
 def main():
-    args = _arg_parser()
-
-    paint(f'Loading ;Cicons;: (mode: ;y{args.type};:)')
-
     
 
 
